75748706c73f833458d13b28c78f05f8bb7c4f5d
TheKelvinT <tnchnkit@hotmail.com>
2023-11-20 20:07:20 +0800

    ui fix

--- src/Components/NotFound/index.tsx
+++ src/Components/NotFound/index.tsx
@@ -9,9 +9,10 @@ const NotFound = () => {
   
   return (
     <div className="main-container">
+             <SpeechBubble/>
       <SideNav />
       <div className="content-container" >
-        <SpeechBubble/>
+ 
     
       </div>
     </div>
